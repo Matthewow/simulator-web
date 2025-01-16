ca4479ae229b4b11696d16b9a6b42a9a38c3e0d0
Matthew W <matthewwang.study@gmail.com>
2025-01-16 18:59:24 +0800

    fix: return type of read file

--- src/components/top_bar/utils.ts
+++ src/components/top_bar/utils.ts
@@ -1,13 +1,13 @@
-export const readFileAsync = (file:Blob) =>{
-    return new Promise((resolve, reject)=>{
-        const fileReader = new FileReader()
-        fileReader.onloadend= (e)=>{
-            if(e?.target?.error){
-                reject("File read failed.")
-            }else{
-                resolve(e?.target?.result)
-            }
-        }
-        fileReader.readAsText(file)
-    })
-}
+export const readFileAsync = (file: Blob): Promise<string> => {
+	return new Promise((resolve, reject) => {
+		const fileReader = new FileReader();
+		fileReader.onloadend = (e) => {
+			if (e?.target?.error) {
+				reject("File read failed.");
+			} else {
+				resolve(e?.target?.result as string);
+			}
+		};
+		fileReader.readAsText(file);
+	});
+};
