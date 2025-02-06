96b6c35c867c0cfbcc610ee86b56a35df9fd69ff
Matthew W <matthewwang.study@gmail.com>
2025-02-06 09:35:42 +0800

    fix: use shared file reader

--- src/components/top_bar/utils.ts
+++ src/components/top_bar/utils.ts
@@ -1,13 +1,14 @@
+const FILE_READER = new FileReader();
+
 export const readFileAsync = (file: Blob): Promise<string> => {
 	return new Promise((resolve, reject) => {
-		const fileReader = new FileReader();
-		fileReader.onloadend = (e) => {
+		FILE_READER.onloadend = (e) => {
 			if (e?.target?.error) {
 				reject("File read failed.");
 			} else {
 				resolve(e?.target?.result as string);
 			}
 		};
-		fileReader.readAsText(file);
+		FILE_READER.readAsText(file);
 	});
 };
