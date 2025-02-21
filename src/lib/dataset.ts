35c62088d49386475ee6b9edc0d8130ac2924d2f
Matthew W <matthewwang.study@gmail.com>
2025-02-21 15:04:47 +0800

    feat: support subway animation

--- src/lib/dataset.ts
+++ src/lib/dataset.ts
@@ -95,19 +95,64 @@ export class Subway implements Transportation {
 	readonly route: Route;
 	readonly sequence: Array<number>;
 	readonly marker: Mesh;
+	readonly lineCode: string;
 
-	constructor(id: string, marker: Mesh) {
+	constructor(id: string, lineCode: string, sequence: number[], marker: Mesh) {
 		this.id = id;
 		this.route = new Map();
+		this.sequence = sequence;
+		this.lineCode = lineCode;
 		this.marker = marker;
-		this.sequence = [];
 	}
 
 	appendRoute(timestamp: number, snapshot: Snapshot): void {
-		throw new Error("Method not implemented.");
+		this.route.set(timestamp, snapshot);
 	}
 	updateMarker(timeInSecond: number, overlay: ThreeJSOverlayView): void {
-		throw new Error("Method not implemented.");
+		const currentTimeIndex = Math.min(
+			Math.floor(timeInSecond / 5),
+			this.sequence.length - 1,
+		);
+		const nextTimeIndex = Math.min(
+			currentTimeIndex + 1,
+			this.sequence.length - 1,
+		);
+
+		const currentTime = this.sequence[currentTimeIndex];
+		const curGeoPosition = this.route.get(currentTime)?.pos;
+
+		const nextTime = this.sequence[nextTimeIndex];
+		const nextGeoPosition = this.route.get(nextTime)?.pos;
+
+		if (curGeoPosition && nextGeoPosition) {
+			const curGlPosition = overlay.latLngAltitudeToVector3(curGeoPosition);
+			const nextGlPosition = overlay.latLngAltitudeToVector3(nextGeoPosition);
+
+			const diff = {
+				x: nextGlPosition.x - curGlPosition.x,
+				z: nextGlPosition.z - curGlPosition.z,
+			};
+
+			const simulatedGlPosition = new Vector3(
+				curGlPosition.x + (diff.x * (timeInSecond % 5)) / 5,
+				curGlPosition.y,
+				curGlPosition.z + (diff.z * (timeInSecond % 5)) / 5,
+			);
+
+			this.marker?.position.copy(simulatedGlPosition);
+
+			if (
+				curGeoPosition.lat !== nextGeoPosition.lat &&
+				curGeoPosition.lng !== nextGeoPosition.lng
+			) {
+				const heading = google.maps.geometry.spherical.computeHeading(
+					curGeoPosition,
+					nextGeoPosition,
+				);
+
+				(this.marker as Mesh).rotation.y = -(heading / 180) * Math.PI;
+			}
+		}
 	}
 }
 
@@ -179,7 +224,7 @@ const parseVehicles = (lines: string[], definitions: Map<string, number>) => {
 			console.error(e);
 		}
 	} else {
-		console.warn("Missing dataset's attributes, bypass following processes.");
+		console.warn("Missing vehicles' attributes, bypass following processes.");
 	}
 
 	return dataset;
@@ -187,6 +232,74 @@ const parseVehicles = (lines: string[], definitions: Map<string, number>) => {
 
 const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 	const dataset: Dataset = { idRouteMap: new Map(), sequence: [] };
+
+	const idIndex = definitions.get("Train ID");
+	const lineCodeIndex = definitions.get("Line Code");
+
+	const posIndex = [
+		definitions.get("Current Station Lat"),
+		definitions.get("Current Station Lng"),
+	];
+	const statusIndex = definitions.get("Current Status");
+	const timestampIndex = definitions.get("Timestamp");
+
+	if (
+		isNumber(idIndex) &&
+		isNumber(lineCodeIndex) &&
+		isNumber(posIndex?.[0]) &&
+		isNumber(posIndex?.[1]) &&
+		isNumber(statusIndex) &&
+		isNumber(timestampIndex)
+	) {
+		try {
+			const idRotueMap = dataset.idRouteMap;
+			const sequence = [] as number[];
+
+			for (const line of lines) {
+				const attributes = line.split(",");
+
+				const id = Number.parseInt(attributes?.[idIndex]);
+				const lineCode = attributes?.[lineCodeIndex];
+				const pos = {
+					lat: Number.parseFloat(attributes?.[posIndex?.[0]]),
+					lng: Number.parseFloat(attributes?.[posIndex?.[1]]),
+				};
+				const timestamp = Number.parseInt(attributes?.[timestampIndex]);
+				const status = attributes?.[statusIndex] as VehicleStatus;
+
+				// Create vehicle or append route if id is valid
+				if (isValidNumber(id)) {
+					if (!idRotueMap.has(id)) {
+						idRotueMap.set(
+							id,
+							new Subway(id, lineCode, sequence, createMarkerMesh()),
+						);
+					}
+
+					const snapshot: Snapshot = { pos, status };
+					idRotueMap.get(id)?.appendRoute(timestamp, snapshot);
+				}
+			}
+
+			const shareSequence = Array.from(
+				new Set(
+					Array.from(dataset.idRouteMap, ([_, vehicle]) =>
+						Array.from(vehicle.route.keys()),
+					).flat(),
+				),
+			).sort() as Array<number>;
+
+			sequence.push(...shareSequence);
+
+			dataset.sequence = sequence;
+		} catch (e) {
+			//In case of parsing error
+			console.error(e);
+		}
+	} else {
+		console.warn("Missing subways' attributes, bypass following processes.");
+	}
+
 	return dataset;
 };
 
