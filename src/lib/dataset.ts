6ccc8e6944045896df1d6161b024a8a127fca0d5
Matthew W <matthewwang.study@gmail.com>
2025-02-28 12:10:59 +0800

    feat: update subway animation

--- src/lib/dataset.ts
+++ src/lib/dataset.ts
@@ -4,11 +4,11 @@ import type { ThreeJSOverlayView } from "@googlemaps/three";
 import { createArrowMesh } from "./marker";
 import type { GeoPosition } from "./types";
 
-export type Snapshot = {
+export type VehicleSnapshot = {
 	pos: GeoPosition;
-	status: VehicleStatus | SubwayStatus;
+	status: VehicleStatus;
 };
-export type Route = Map<number, Snapshot>;
+export type Route = Map<number, VehicleSnapshot | SubwaySnapshot>;
 
 export interface Transportation {
 	updateMarker(timeInSecond: number, overlay: ThreeJSOverlayView): void;
@@ -17,11 +17,12 @@ export interface Transportation {
 
 export type VehicleType = "Taxi" | "Private Car";
 export type VehicleStatus = "EMPTY" | "OFFLINE" | "BOARDING";
+export type VehicleRoute = Map<number, VehicleSnapshot>;
 export class Vehicle implements Transportation {
 	readonly id: number;
 
 	readonly vtype: VehicleType;
-	readonly route: Route;
+	readonly route: VehicleRoute;
 
 	sequence: Array<number>;
 	marker: Mesh;
@@ -34,7 +35,7 @@ export class Vehicle implements Transportation {
 		this.marker = marker;
 	}
 
-	appendRoute(timestamp: number, snapshot: Snapshot) {
+	appendRoute(timestamp: number, snapshot: VehicleSnapshot) {
 		this.route.set(timestamp, snapshot);
 	}
 
@@ -89,14 +90,14 @@ export class Vehicle implements Transportation {
 export type SubwayStatus = "BOARDING" | "RUNNING";
 export type SubwayRecord = { timestamp: number; status: SubwayStatus };
 export type SubwaySnapshot = {
-	timestamp: number;
-	pos: GeoPosition;
+	progress: number;
 	startPos: GeoPosition;
 	endPos: GeoPosition;
 };
+export type SubwayRoute = Map<number, SubwaySnapshot>;
 export class Subway implements Transportation {
 	readonly id: number;
-	readonly route: Route;
+	readonly route: SubwayRoute;
 	readonly sequence: Array<number>;
 	readonly marker: Mesh;
 	readonly lineCode: string;
@@ -109,7 +110,7 @@ export class Subway implements Transportation {
 		this.marker = marker;
 	}
 
-	appendRoute(timestamp: number, snapshot: Snapshot): void {
+	appendRoute(timestamp: number, snapshot: SubwaySnapshot): void {
 		this.route.set(timestamp, snapshot);
 	}
 	updateMarker(timeInSecond: number, overlay: ThreeJSOverlayView): void {
@@ -117,45 +118,40 @@ export class Subway implements Transportation {
 			Math.floor(timeInSecond / 5),
 			this.sequence.length - 1,
 		);
-		const nextTimeIndex = Math.min(
-			currentTimeIndex + 1,
-			this.sequence.length - 1,
-		);
+
 
 		const currentTime = this.sequence[currentTimeIndex];
-		const curGeoPosition = this.route.get(currentTime)?.pos;
+		const progress = this.route.get(currentTime)?.progress;
 
-		const nextTime = this.sequence[nextTimeIndex];
-		const nextGeoPosition = this.route.get(nextTime)?.pos;
+		const startPosition = this.route.get(currentTime)?.startPos;
+		const endPosition = this.route.get(currentTime)?.endPos;
 
-		if (curGeoPosition && nextGeoPosition) {
-			const curGlPosition = overlay.latLngAltitudeToVector3(curGeoPosition);
-			const nextGlPosition = overlay.latLngAltitudeToVector3(nextGeoPosition);
+
+		if (progress && startPosition && endPosition) {
+			const startGlPosition = overlay.latLngAltitudeToVector3(startPosition);
+			const endGlPosition = overlay.latLngAltitudeToVector3(endPosition);
 
 			const diff = {
-				x: nextGlPosition.x - curGlPosition.x,
-				z: nextGlPosition.z - curGlPosition.z,
+				x: endGlPosition.x - startGlPosition.x,
+				z: endGlPosition.z - startGlPosition.z,
 			};
 
 			const simulatedGlPosition = new Vector3(
-				curGlPosition.x + (diff.x * (timeInSecond % 5)) / 5,
-				curGlPosition.y,
-				curGlPosition.z + (diff.z * (timeInSecond % 5)) / 5,
+				startGlPosition.x + diff.x * progress,
+				startGlPosition.y,
+				startGlPosition.z + diff.z * progress,
 			);
 
 			this.marker?.position.copy(simulatedGlPosition);
 
-			if (
-				curGeoPosition.lat !== nextGeoPosition.lat &&
-				curGeoPosition.lng !== nextGeoPosition.lng
-			) {
-				const heading = google.maps.geometry.spherical.computeHeading(
-					curGeoPosition,
-					nextGeoPosition,
-				);
 
-				(this.marker as Mesh).rotation.y = -(heading / 180) * Math.PI;
-			}
+			const heading = google.maps.geometry.spherical.computeHeading(
+				startPosition,
+				endPosition,
+			);
+
+			(this.marker as Mesh).rotation.y = -(heading / 180) * Math.PI;
+
 		}
 	}
 }
@@ -207,7 +203,7 @@ const parseVehicles = (lines: string[], definitions: Map<string, number>) => {
 						);
 					}
 
-					const snapshot: Snapshot = { pos, status };
+					const snapshot = { pos, status };
 					idVehicleMap.get(id)?.appendRoute(timestamp, snapshot);
 				}
 			}
@@ -253,6 +249,10 @@ const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 	];
 	const statusIndex = definitions.get("Current Status");
 	const timestampIndex = definitions.get("Timestamp");
+	const durationIndex = [
+		definitions.get("Time to Departure"),
+		definitions.get("Time to Next Station"),
+	]
 
 	if (
 		isNumber(idIndex) &&
@@ -262,10 +262,12 @@ const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 		isNumber(nextStationIndex?.[0]) &&
 		isNumber(nextStationIndex?.[1]) &&
 		isNumber(statusIndex) &&
-		isNumber(timestampIndex)
+		isNumber(timestampIndex) &&
+		isNumber(durationIndex?.[0]) &&
+		isNumber(durationIndex?.[1])
 	) {
 		try {
-			const idSubwayMap = dataset.idRouteMap;
+			const idSubwayMap = dataset.idRouteMap as Map<number, Subway>;
 			const sequence = [] as number[];
 
 			for (const line of lines) {
@@ -285,7 +287,11 @@ const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 					lat: Number.parseFloat(attributes?.[nextStationIndex?.[0]]),
 					lng: Number.parseFloat(attributes?.[nextStationIndex?.[1]]),
 				};
-				const pos = status === "BOARDING" ? startPos : endPos;
+
+				const time2Start = Number.parseFloat(attributes?.[durationIndex?.[0]]);
+				const time2Arrive = Number.parseFloat(attributes?.[durationIndex?.[1]]);
+
+				const progress = status === "BOARDING" ? 0 : (timestamp - time2Start) / (time2Arrive - time2Start);
 
 				// Create vehicle or append route if id is valid
 				if (isValidNumber(id)) {
@@ -296,14 +302,14 @@ const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 						);
 					}
 
-					const snapshot = { pos, status, startPos, endPos };
+					const snapshot = { status, startPos, endPos, progress };
 					idSubwayMap.get(id)?.appendRoute(timestamp, snapshot);
 				}
 			}
 
 			const shareSequence = Array.from(
 				new Set<number>(
-					Array.from(dataset.idRouteMap, ([_, vehicle]) =>
+					Array.from(idSubwayMap, ([_, vehicle]) =>
 						Array.from(vehicle.route.keys()) as number[],
 					).flat(),
 				),
@@ -314,64 +320,6 @@ const parseSubways = (lines: string[], definitions: Map<string, number>) => {
 			sequence.push(...shareSequence);
 
 			dataset.sequence = sequence;
-
-			for (const subway of idSubwayMap.values()) {
-				const statusPeriod: Array<SubwayRecord> = [];
-				for (const [timestamp, snapshot] of subway.route.entries()) {
-					if (statusPeriod.length === 0) {
-						statusPeriod.push({
-							timestamp,
-							status: snapshot.status as SubwayStatus,
-						});
-						continue;
-					}
-
-					if (
-						snapshot.status !== statusPeriod[statusPeriod.length - 1].status
-					) {
-						statusPeriod.push({
-							timestamp,
-							status: snapshot.status as SubwayStatus,
-						});
-					} else {
-						statusPeriod[statusPeriod.length - 1] = {
-							timestamp,
-							status: snapshot.status as SubwayStatus,
-						};
-					}
-				}
-				statusPeriod;
-
-				for (const [timestamp, snapshot] of subway.route.entries()) {
-					const tripStartIndex = statusPeriod.findIndex(
-						(record) => record.timestamp < timestamp,
-					);
-					if (
-						tripStartIndex === -1 ||
-						tripStartIndex === statusPeriod.length - 1
-					) {
-						continue;
-					}
-
-					const { timestamp: startTimestamp } = statusPeriod[tripStartIndex];
-
-					const tripEndIndex = tripStartIndex + 1;
-					const { timestamp: endTimestamp, status: endStatus } =
-						statusPeriod[tripEndIndex];
-
-					if (endStatus === "RUNNING") {
-						const subwaySnapshot = snapshot as unknown as SubwaySnapshot;
-						const { startPos, endPos } = subwaySnapshot;
-						const duration = endTimestamp - startTimestamp;
-						const timeRatio = (timestamp - startTimestamp) / duration;
-
-						subwaySnapshot.pos = {
-							lat: startPos.lat + (endPos.lat - startPos.lat) * timeRatio,
-							lng: startPos.lng + (endPos.lng - startPos.lng) * timeRatio,
-						};
-					}
-				}
-			}
 		} catch (e) {
 			//In case of parsing error
 			console.error(e);
