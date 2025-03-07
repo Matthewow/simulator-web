9289da790f0ca2b86d85af6718da4053a1dc1e67
Matthew W <matthewwang.study@gmail.com>
2025-03-07 13:25:11 +0800

    fix: correct metro station with data from stations.json

--- src/lib/railway.ts
+++ src/lib/railway.ts
@@ -1,6 +1,7 @@
 import type { ThreeJSOverlayView } from "@googlemaps/three";
 import type { GeoPosition } from "./types";
 import METRO from "@/assets/metro.json";
+import STATIONS from "@/assets/station.json";
 import { createCircleMesh } from "./marker";
 
 export type Station = {
@@ -10,28 +11,42 @@ export type Station = {
 
 export const MTR_STATION_MAP: Map<string, Station> = new Map();
 
-const convertCSV2Station = (csvLine: string) => {
-	const elements = csvLine.split(",");
-	const stationCode = elements[2];
-	const stationGeoPos = {
-		lat: Number.parseFloat(elements[7]),
-		lng: Number.parseFloat(elements[8]),
-	};
-	MTR_STATION_MAP.set(stationCode, { pos: stationGeoPos, route: new Map() });
-};
+const getNameAbbrMap = (): Map<string, string> => {
+	const nameCodeMap = new Map<string, string>();
+	for (const csvLine of METRO.mtr) {
+		const elements = csvLine.split(",");
 
-for (const csvLine of METRO.mtr) {
-	convertCSV2Station(csvLine);
-}
+		const stationCode = elements[2];
+		const stationName = elements[5];
+
+		nameCodeMap.set(stationName, stationCode);
+	}
+
+	return nameCodeMap;
+};
 
 export const appendRailwayLayer = (overlay: ThreeJSOverlayView) => {
 	for (const [_, station] of MTR_STATION_MAP) {
 		const geoLocation = station.pos;
 		const glLocation = overlay.latLngAltitudeToVector3(geoLocation);
-
 		const mesh = createCircleMesh();
 		mesh.position.copy(glLocation);
-
 		overlay.scene.add(mesh);
 	}
 };
+
+// Get Abbr and Full name map from metro.json
+const nameCodeMap = getNameAbbrMap();
+
+// Get Station Geolocation from staion.json
+for (const station of STATIONS) {
+	const name = station.properties.name;
+	if (nameCodeMap.has(name)) {
+		const abbr = nameCodeMap.get(name) as string;
+		const geoLocation = station.geometry.coordinates;
+		MTR_STATION_MAP.set(abbr, {
+			pos: { lat: geoLocation[1], lng: geoLocation[0] },
+			route: new Map(),
+		});
+	}
+}
