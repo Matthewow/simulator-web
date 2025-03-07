91a9f2e9fe81f71d709a5ff48d3f80d1d843fc41
Matthew W <matthewwang.study@gmail.com>
2025-03-07 10:08:41 +0800

    fix: typo

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const rename = require('gulp-rename');
 
 // Define a task to move env.json to src/features/config.json and rename it
 gulp.task('move-env',  ()=> {
-    return gulp.src('./env/screct.local.json')
+    return gulp.src('./env/secret.local.json')
         .pipe(rename('secret.json'))  // Rename the file to config.json
         .pipe(gulp.dest('./src/assets/'));  // Move to the destination
 });
