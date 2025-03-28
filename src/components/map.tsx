bb313d5b2464b7b9c772e73b55c6167a356bc81b
Matthew W <matthewwang.study@gmail.com>
2025-03-28 18:11:08 +0800

    chore: add matcap

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import { initGoogleMap } from "@/lib/render";
-import { preparePLYs, prepareSVGs } from "@/lib/marker";
+import { preparePLYs, prepareTexture } from "@/lib/marker";
 import { loadDataSet } from "@/lib/dataset";
 import { useAppstore } from "@/store";
 
@@ -10,7 +10,7 @@ const MapProvider = () => {
 	useEffect(() => {
 		(async () => {
 			setDataStatus("Loading");
-			await Promise.all([initGoogleMap(), prepareSVGs(), preparePLYs()]);
+			await Promise.all([initGoogleMap(),  preparePLYs(), prepareTexture()]);
 			const dataset = await loadDataSet();
 			setDataset(dataset);
 		})();
--- src/lib/marker.ts
+++ src/lib/marker.ts
@@ -1,21 +1,15 @@
 import {
-	type Box3,
 	type BufferGeometry,
 	CircleGeometry,
-	Color,
-	DoubleSide,
-	Group,
 	Mesh,
 	MeshBasicMaterial,
 	MeshMatcapMaterial,
 	type Object3D,
-	ShapeGeometry,
+	SRGBColorSpace,
+	type Texture,
+	TextureLoader,
 } from "three";
-import {
-	PLYLoader,
-	SVGLoader,
-	type SVGResult,
-} from "three/examples/jsm/Addons.js";
+import { PLYLoader } from "three/examples/jsm/Addons.js";
 import type { VehicleStatus, SubwayStatus } from "./types";
 
 const DEFAULT_MATERIAL = new MeshBasicMaterial({ color: 0x000000 });
@@ -28,40 +22,18 @@ export const createCircleMesh = () => {
 };
 
 export const MARKER_COLOR_MAP = {
-	BOARDING: "#333399",
-	EMPTY: "#333333",
+	BOARDING: "#555599",
+	EMPTY: "#555555",
 	OFFLINE: "#999999",
-	RUNNING: "#339933",
-	PICKUP: "#ffff33",
+	RUNNING: "#559955",
+	PICKUP: "#ffff55",
 } as {
 	[key in VehicleStatus | SubwayStatus]: string;
 };
 
 const SVG_NAMES = ["subway", "private car", "taxi", "bus"];
-const DEFAULT_SVG_GEOMETRIES = new Map<string, ShapeGeometry[]>();
 const DEFAULT_PLY_GEOMETRIES = new Map<string, BufferGeometry>();
-
-export const createSVGGroup = (type: string) => {
-	const group = new Group();
-	const geometries = DEFAULT_SVG_GEOMETRIES.get(type.toLocaleLowerCase());
-
-	if (!geometries) {
-		throw new Error("Unsupported types");
-	}
-
-	const material = new MeshBasicMaterial({
-		color: new Color(0x000000),
-		side: DoubleSide,
-		depthWrite: false,
-	});
-
-	for (const geometry of geometries as ShapeGeometry[]) {
-		const mesh = new Mesh(geometry, material);
-		group.add(mesh);
-	}
-
-	return group;
-};
+let MATCAP_TEXTURE: Texture | null = null;
 
 export const createPLYGroup = (type: string) => {
 	const geometry = DEFAULT_PLY_GEOMETRIES.get(type.toLocaleLowerCase());
@@ -70,7 +42,9 @@ export const createPLYGroup = (type: string) => {
 		throw new Error("Unsupported types");
 	}
 
-	const material = new MeshMatcapMaterial({});
+	const material = new MeshMatcapMaterial({
+		matcap: MATCAP_TEXTURE as Texture,
+	});
 
 	return new Mesh(geometry, material);
 };
@@ -84,38 +58,10 @@ export const setGroupMaterialColorByStatus = (
 	((object as Mesh).material as MeshBasicMaterial).color.set(rgbStr);
 };
 
-const generateSVGGroup = (data: SVGResult) => {
-	const paths = data.paths;
-
-	const geometries = paths.flatMap((path) => {
-		const shapes = SVGLoader.createShapes(path);
-		const geometries = shapes.map((shape) => {
-			const geometry = new ShapeGeometry(shape);
-			geometry.computeBoundingBox();
-			geometry.translate(
-				-(geometry.boundingBox as Box3).max.x * 0.5,
-				-(geometry.boundingBox as Box3).max.y * 0.5,
-				-(geometry.boundingBox as Box3).max.z * 0.5,
-			);
-
-			geometry.rotateX(Math.PI / 2);
-			geometry.scale(0.05, 0.05, 0.05);
-
-			return geometry;
-		});
-
-		return geometries;
-	});
-
-	return geometries;
-};
-
-export const prepareSVGs = async () => {
-	const loader = new SVGLoader();
-	for (const name of SVG_NAMES) {
-		const res = await loader.loadAsync(`/${name}.svg`);
-		DEFAULT_SVG_GEOMETRIES.set(name, generateSVGGroup(res));
-	}
+export const prepareTexture = async () => {
+	const textureLoader = new TextureLoader();
+	MATCAP_TEXTURE = await textureLoader.loadAsync("./matcap_3.png");
+	MATCAP_TEXTURE.colorSpace = SRGBColorSpace;
 };
 
 export const preparePLYs = async () => {
