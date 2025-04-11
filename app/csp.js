30770b670069a44b5252c006b109177446ba9abf
Matthew W <matthewwang.study@gmail.com>
2025-04-11 11:06:35 +0800

    fix: append csp rules to allow google api and wam

--- app/csp.js
+++ app/csp.js
@@ -6,7 +6,7 @@ const setupCSP = () => {
 			responseHeaders: {
 				...details.responseHeaders,
 				"Content-Security-Policy": [
-					"default-src 'self'; script-src 'self';style-src 'self' 'unsafe-inline';",
+					"default-src 'self'; script-src 'self' https://maps.googleapis.com 'wasm-unsafe-eval'; style-src 'self' https://*.googleapis.com 'unsafe-inline'; connect-src 'self' data: https://*.googleapis.com https://maps.gstatic.com; worker-src 'self' blob:; img-src 'self' data: https://*.googleapis.com https://*.gstatic.com data:; font-src 'self' https://fonts.gstatic.com;",
 				],
 			},
 		});
